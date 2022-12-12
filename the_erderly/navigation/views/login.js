d7ae9979679344206d1051befb64f21a4ee49d4f
gihyeok Kim <gh9812301@naver.com>
2022-12-12 23:37:31 +0900

    loginpage

--- the_erderly/navigation/views/login.js
+++ the_erderly/navigation/views/login.js
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View, Alert } from 'react-native';
+import { Pressable, StyleSheet, Text, View, Alert ,TouchableOpacity} from 'react-native';
 import React, { useState } from 'react';
 import {
   KakaoOAuthToken,
@@ -7,11 +7,14 @@ import {
   login,
   logout,
   unlink,
+  
 } from '@react-native-seoul/kakao-login';
 import IntroView from './IntroView';
 import database from '@react-native-firebase/database';
 import { useSelector, useDispatch } from 'react-redux';
 import { setId } from '../redux/action';
+import EmlogoSvg from '../../src/svgFile/emlogo.svg';
+
 
 export default function Kakaologin({navigation}) {
   const {id} = useSelector(state => state.userReducer);
@@ -82,30 +85,69 @@ export default function Kakaologin({navigation}) {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={{fontSize:30, fontWeight:'bold', marginTop:200, marginRight:200, color:'black'}}>로그인</Text>
-      {/* <IntroView result={nickname} /> */}
-      <Pressable
-        style={styles.button}
-        onPress={() => {
-          signInWithKakao();
-        }}
-      >
-        <Text style={{textAlign:'center', fontSize:30}}>
-          카카오 로그인
-        </Text>
-      </Pressable>
-      <Pressable
-        style={styles.button2}
-        onPress={() => {
-          navigation.navigate('EmLogin')
-        }}
-      >
-        <Text style={{color:'white', textAlign:'center', fontSize:30}}>
-          이음 간편 로그인
-        </Text>
-      </Pressable>
-      {/* <Pressable
+    <View style={{flex:1,justifyContent:'center',alignContent:"center"}}>
+      <EmlogoSvg style={{position:"absolute",top:131,left:71}}></EmlogoSvg>
+      <Text style={{position:"absolute",top:178.92,left:70.06,color:"#4E4E4E",fontSize:25,fontWeight:"bold"}}>로그인</Text>
+      <TouchableOpacity style={{backgroundColor:"#FFC700",width:290.62,height:66.99,borderRadius:15,position:"absolute",top:280.47,left:60.69,justifyContent:"center"}}  onPress={() => {
+           signInWithKakao();
+         }}>
+        <Text style={{alignSelf:"center",fontSize:20,fontWeight:"bold",color:"white"}}>카카오 로그인</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={{backgroundColor:"#0DC540",width:290.62,height:66.99,borderRadius:15,position:"absolute",top:396.16,left:60.69,justifyContent:"center"}}>
+        <Text style={{alignSelf:"center",fontSize:20,fontWeight:"bold",color:"white"}}>네이버 로그인</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={{backgroundColor:"#676767",width:290.62,height:66.99,borderRadius:15,position:"absolute",top:511.85,left:60.69,justifyContent:"center"}} onPress={() => {
+           navigation.navigate('EmLogin')
+         }}>
+        <Text style={{alignSelf:"center",fontSize:20,fontWeight:"bold",color:"white"}}>전화번호 로그인 하기</Text>
+      </TouchableOpacity>
+
+      <Text style={{color:"#C4C4C4",position:"absolute",top:661.36,left:130.42}}>
+        서비스 시작 시 사용 관련   {'\n'}    알림이 전송됩니다.
+      </Text>
+
+
+    </View>
+
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+    // <View style={styles.container}>
+    //   <Text style={{fontSize:30, fontWeight:'bold', marginTop:200, marginRight:200, color:'black'}}>로그인</Text>
+    //   {/* <IntroView result={nickname} /> */}
+    //   <Pressable
+    //     style={styles.button}
+    //     onPress={() => {
+    //       signInWithKakao();
+    //     }}
+    //   >
+    //     <Text style={{textAlign:'center', fontSize:30}}>
+    //       카카오 로그인
+    //     </Text>
+    //   </Pressable>
+    //   <Pressable
+    //     style={styles.button2}
+    //     onPress={() => {
+    //       navigation.navigate('EmLogin')
+    //     }}
+    //   >
+    //     <Text style={{color:'white', textAlign:'center', fontSize:30}}>
+    //       이음 간편 로그인
+    //     </Text>
+    //   </Pressable>
+      /* <Pressable
         style={styles.button}
         onPress={() => getProfile()}
       >
@@ -128,9 +170,8 @@ export default function Kakaologin({navigation}) {
         <Text style={styles.text}>
           카카오 로그아웃
         </Text>
-      </Pressable> */}
-    </View>
-  );
+      </Pressable> */   
+  
 }
 
 const styles = StyleSheet.create({
