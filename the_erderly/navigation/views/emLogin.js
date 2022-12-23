926c835ce0ad64bad7b5d96f5503d2402d476ed9
nahyeonung <ntweety@naver.com>
2022-12-23 18:11:56 +0900

    이음 로그인 페이지 디자인 수정

--- the_erderly/navigation/views/emLogin.js
+++ the_erderly/navigation/views/emLogin.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from 'react';
-import { StyleSheet, View, Text, Button, Pressable, TextInput, Alert } from 'react-native';
+import { StyleSheet, View, Text, Button, Pressable, TextInput, Alert,TouchableOpacity } from 'react-native';
 import database from '@react-native-firebase/database';
 import RectangleSvg from '../../src/svgFile/Rectangle.svg';
 import RegisterSvg from '../../src/svgFile/register.svg';
@@ -63,29 +63,29 @@ export default function EmLogin({navigation}) {
     return(
         <View style={{flex:1}}>
             <View style={{flex: 1,alignItems:'center', justifyContent:'center'}}>
-                <Text style={{fontSize: 30, fontWeight:'bold'}}>로그인</Text>
+                <Text style={{fontSize: 30, fontWeight:'bold', color:'#4E4E4E', marginRight:'50%'}}>로그인</Text>
             </View>
-            <View style={{flex: 0.5,alignItems:'center', justifyContent:'center'}}>
-                <TextInput style={styles.input} placeholder="아이디" value={pnumber}
+            <View style={{flex: 0.5,marginTop:'20%' ,alignItems:'center', justifyContent:'center'}}>
+                <TextInput style={styles.input} placeholder="전화번호" value={pnumber}
                 onChangeText={text => onChangeP(text)}/>
                 <TextInput style={styles.input} placeholder="비밀번호" value={number}
                 onChangeText={text => onChange(text)}/>
-                <Pressable
-                    style={styles.button}
+                <TouchableOpacity
+                    style={{backgroundColor:"#03CF5D",width:288.02,height:55,borderRadius:15,justifyContent:"center"}}
                     title="To User Screen"
                         onPress={() => login()}>
-                    <Text style={{fontSize:20, fontWeight:'bold'}}>로그인</Text>
-                </Pressable>
+                    <Text style={{alignSelf:'center',fontSize:20,fontWeight:"bold",color:"white"}}>로그인</Text>
+                </TouchableOpacity>
             </View>
-            <View style={{flex: 1,alignItems:'center', justifyContent:'center'}}>
-                <Pressable
-                    style={styles.button}
+            <View style={{flex: 1,marginTop:'10%' ,alignItems:'center', justifyContent:'center'}}>
+                <TouchableOpacity
+                    style={{backgroundColor:"#03CF5D",width:288.02,height:55,borderRadius:15,justifyContent:"center"}}
                     title="To User Screen"
                         onPress={() => navigation.navigate('Registration')}>
-                    <Text style={{fontSize:20, fontWeight:'bold'}}>회원가입 하기</Text>
-                </Pressable>
+                    <Text style={{alignSelf:"center",fontSize:20,fontWeight:"bold",color:"white"}}>회원가입</Text>
+                </TouchableOpacity>
             </View>
-            <Button onPress={() => navigation.navigate('View2')} title="넘기기" />
+            {/* <Button onPress={() => navigation.navigate('View2')} title="넘기기" /> */}
         </View>
     )
 }
@@ -103,8 +103,8 @@ const styles = StyleSheet.create({
         fontSize: '20px',
     },
     input: {
-        width: "70%",
-        height: 60,
+        width: 288.02,
+        height: 55,
         borderColor: '#cecece',
         backgroundColor: '#cecece',
         borderWidth: 1,
@@ -115,17 +115,4 @@ const styles = StyleSheet.create({
         fontWeight:'bold',
         fontSize:20
       },
-    button: {
-        width: "70%",
-        height: 60,
-        borderColor: '#03CF5D',
-        backgroundColor: '#03CF5D',
-        borderWidth: 1,
-        borderRadius: 15,
-        padding: 10,
-        marginRight: 10,
-        marginBottom: 15,
-        fontWeight:'bold',
-        alignItems:'center',
-    },
 })
